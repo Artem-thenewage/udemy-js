740edef456e519b238fc9d5c2646c76a40a9996d
Artemiy <craftsman2004@mail.ru>
2022-07-30 10:38:58 +0300

    Coding Exercise 10: Задачи на работу с объектами

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,56 @@
-function fib(x) {
+// 1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать 
+// строку с опытом.
+// Пример:
+// showExperience(personalPlanPeter) => '1 month'
+// P.S. желательно использовать деструктуризацию, но не обязательно
+// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и 
+// возвращать строку в нужном виде.
+// Пример:
+// showProgrammingLangs(personalPlanPeter)  =>
+// "Язык js изучен на 20% Язык php изучен на 10%"
+// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то 
+// возвращается пустая строка.
+// P.S. Для переноса строки используется \n в конце строки.
+// 3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать 
+// в себя объект и возвращать строку в нужном виде.
+// Пример:
+// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
+// => 'Мне 29 и я владею языками: RU ENG'
+// Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в верхнем 
+// регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
 
-    if (x <= 0 || typeof(x)!='number') {
-        return "";
-    }
-    let res = '0';
-    let prev1 = 0, prev2 = 1, sum = 0;
-    for (let i = 2; i <= x; i++) {
-        if (i == 2) {
-            sum = 1;
-            res += ' ' + sum;
-        }
-        else {
-            sum = prev1 + prev2;
-            res += ' ' + sum;
-            prev1 = prev2;
-            prev2 = sum;
-        }
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%',
+            c:'24%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(obj) {
+        return `Мне ${obj.age} и я владею языками: ${obj.skills.languages.join(" ").toUpperCase()}`;
+    } 
+};
+
+//console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+
+
+function showExperience(plan) {
+    return plan.skills.exp;
+}
+//console.log(showExperience(personalPlanPeter));
+
+
+function showProgrammingLangs(plan) {
+    const programmingLangs = plan.skills.programmingLangs;
+    let res = '';
+    for(let key in programmingLangs){
+        res += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
     }
     return res;
 }
-
-console.log(fib(7));
+console.log(showProgrammingLangs(personalPlanPeter));
