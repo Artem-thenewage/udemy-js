4bfea8fc0d93190c357f525edcfa87c7fc9551f0
Artemiy <craftsman2004@mail.ru>
2022-07-30 10:57:17 +0300

    Coding Exercise 11: Задачи на работу с массивами

--- JS/script.js
+++ JS/script.js
@@ -1,56 +1,33 @@
-// 1) Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать 
-// строку с опытом.
+
+// Задачи:
+// 1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в 
+// нужном формате.
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+// Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+// 2) напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет 
+// выводить в консоль эти строки в нижнем регистре.
 // Пример:
-// showExperience(personalPlanPeter) => '1 month'
-// P.S. желательно использовать деструктуризацию, но не обязательно
-// 2) Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и 
-// возвращать строку в нужном виде.
-// Пример:
-// showProgrammingLangs(personalPlanPeter)  =>
-// "Язык js изучен на 20% Язык php изучен на 10%"
-// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то 
-// возвращается пустая строка.
-// P.S. Для переноса строки используется \n в конце строки.
-// 3) Создайте метод showAgeAndLangs внутри объекта personalPlanPeter. При его вызове метод будет принимать 
-// в себя объект и возвращать строку в нужном виде.
-// Пример:
-// personalPlanPeter.showAgeAndLangs(personalPlanPeter)
-// => 'Мне 29 и я владею языками: RU ENG'
-// Заметьте, что возраст и языки подставляются автоматически из объекта, а языки всегда в верхнем 
-// регистре (большими буквами). Если данные в объекте поменяются, то и сообщение тоже изменится.
+// standardizeStrings(favoriteCities)  выведет в консоль
+// lisbon
+// rome
+// milan
+// dublin
+// Это частая задача в реальности, так как от пользователя нам могут прийти ответы в самых разных 
+// форматах. В том числе и с разными буквами :) Поэтому нам нужно привести строки в один формат для правильной работы.
+'use strict';
 
-const personalPlanPeter = {
-    name: "Peter",
-    age: "29",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%',
-            c:'24%'
-        },
-        exp: '1 month'
-    },
-    showAgeAndLangs: function(obj) {
-        return `Мне ${obj.age} и я владею языками: ${obj.skills.languages.join(" ").toUpperCase()}`;
-    } 
-};
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
-//console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
+function showFamily(arr) {
+    if (arr.length === 0) {return "Семья пуста";}
+    return `Семья состоит из: ${arr.join(' ')}`;
 
-
-function showExperience(plan) {
-    return plan.skills.exp;
 }
-//console.log(showExperience(personalPlanPeter));
+console.log(showFamily(family));
 
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
 
-function showProgrammingLangs(plan) {
-    const programmingLangs = plan.skills.programmingLangs;
-    let res = '';
-    for(let key in programmingLangs){
-        res += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
-    }
-    return res;
+function standardizeStrings(arr) {
+    
 }
-console.log(showProgrammingLangs(personalPlanPeter));
+
