bda8db3f46d878b363bc39081c304aace20c5422
Artemiy <craftsman2004@mail.ru>
2022-08-08 19:17:01 +0300

    Coding Exercise 15: Задания на поиск ошибок в коде

--- JS/script.js
+++ JS/script.js
@@ -1,54 +1,54 @@
-// Задача:
-// У вас есть список учеников, которые хотят поиграть в игру:
-// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
-// Но команд может быть только 3 по 3 человека. Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
-// Внутри она сначала сортирует имена по алфавиту. Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку. 
-// Эти группы должны быть массивами. Как итог, функция возвращает новый массив с тремя командами и строкой как 4й элемент.
-// Пример:
-// sortStudentsByGroups(students)  =>
-// [
-//   [ 'Andrew', 'Ann', 'Bernard' ],
-//   [ 'Cris', 'Josh', 'Mark' ],
-//   [ 'Peter', 'Sam', 'Sandra' ],
-//   'Оставшиеся студенты: Takesi'
-// ]
-// Если убрать одно студента из списка, то результат будет:
-// [
-//   [ 'Andrew', 'Ann', 'Bernard' ],
-//   [ 'Cris', 'Josh', 'Mark' ],
-//   [ 'Peter', 'Sam', 'Sandra' ],
-//   'Оставшиеся студенты: -'
-// ]
-// А если добавить одного, то:
-// [
-//   [ 'Andrew', 'Ann', 'Bernard' ],
-//   [ 'Cris', 'Josh', 'Mark' ],
-//   [ 'Peter', 'Sam', 'Sandra' ],
-//   'Оставшиеся студенты: Takesi, Somebody'
-// ]
-// То есть, меняется содержимое строки. Все оставшиеся ученики попадают туда.
-// Задача интересная, немного заковыристая, но все необходимое для неё мы уже проходили. Просто распишите логику действий строка за строкой.
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam', 'Bernard'];
+function isOpen(prop) {
+    let answer = '';
+    !prop ? answer = 'Закрыто' : answer = 'Открыто';
 
-function sortStudentsByGroups(arr) {
-
-    const students = arr.sort().filter(item => item.id == null);
-    const groups = 3, inGroup = 3;
-    let studentGroups = [], outGroup = 0;
-
-    outGroup = (students.length % groups);
-
-    for (let i = 0; i < groups; i++) {
-        studentGroups[i] = students.slice(i * inGroup, i * inGroup + inGroup);
-    }
-    
-    studentGroups[groups] = `Оставшиеся студенты: -`;
-
-    if (outGroup > 0) {
-        studentGroups[groups] = `Оставшиеся студенты: ${students.slice(-outGroup).join(', ')}`;  
-    }
-    return studentGroups;
+    return answer;
 }
 
-console.log(sortStudentsByGroups(students));
+//console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < parseInt(average,10)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+//console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+
+    copy.waitors = copy.waitors.slice();
+    copy.waitors[0] = {name: 'Mike', age: 32};
+    return copy;
+}
+
+console.log(transferWaitors(restorantData));
+console.log(restorantData);
