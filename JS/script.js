3f1952d43a97f046a3c63d28057890269aec2b21
Artemiy <craftsman2004@mail.ru>
2022-08-19 13:41:21 +0300

    Coding Exercise 16: (*) Задача на рекурсию

--- JS/script.js
+++ JS/script.js
@@ -1,54 +1,33 @@
-const restorantData = {
-    menu: [
-        {
-            name: 'Salad Caesar',
-            price: '14$'
-        },
-        {
-            name: 'Pizza Diavola',
-            price: '9$'
-        },
-        {
-            name: 'Beefsteak',
-            price: '17$'
-        },
-        {
-            name: 'Napoleon',
-            price: '7$'
-        }
-    ],
-    waitors: [
-        {name: 'Alice', age: 22}, {name: 'John', age: 24}
-    ],
-    averageLunchPrice: '20$',
-    openNow: true
-};
+// (*) Задача на рекурсию
+// Здесь мы с вами рассмотрим одну из классических задач на рекурсию, которую дают на собеседованиях. Звучит она очень просто:
+// Напишите функцию, которая вычисляет факториал.
+// Задание простое, но нужно понимать что такое факториал вообще. Факториал  – это число, умноженное на "себя минус один", 
+// затем на "себя минус два", и так далее до 1. Факториал n обозначается как n!
+// Отсюда мы можем понять, что функция должна принимать 1 аргумент, который будет являться числом. Будет неплохо, 
+// если вы на собеседовании сразу напишите проверку на приходящее значение :) Поэтому, если в нашу функцию приходит 
+// дробное число или не число  - возвращается строка с любым сообщением на ваше усмотрение. Если 0 и меньше - возвращается число 1.
+// Сам же факториал с примерами выглядит вот так:
+// n! = n * (n - 1) * (n - 2) * ...* 1 - это общая формула
+// Примеры значений для разных n:
+// 1! = 1
+// 2! = 2 * 1 = 2
+// 3! = 3 * 2 * 1 = 6
+// 4! = 4 * 3 * 2 * 1 = 24
+// 5! = 5 * 4 * 3 * 2 * 1 = 120
+// То есть, вызов нашей функции factorial(5) возвращает число 120
+// factorial(4) => 24
+// Решить задачу нужно через рекурсию.
+"use strict";
 
-function isOpen(prop) {
-    let answer = '';
-    !prop ? answer = 'Закрыто' : answer = 'Открыто';
-
-    return answer;
-}
-
-//console.log(isOpen(restorantData.openNow));
-
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < parseInt(average,10)) {
-        return 'Цена ниже средней';
-    } else {
-        return 'Цена выше средней';
+function factorial(n) {
+    if ( typeof(n) != 'number' || n % 1 > 0 ) {return "неправильное число";}
+    if ( n <= 0 ) {return 1;}
+    if (n === 1) {
+        return 1;
     }
-}
-//console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
-
-function transferWaitors(data) {
-    const copy = Object.assign({}, data);
-
-    copy.waitors = copy.waitors.slice();
-    copy.waitors[0] = {name: 'Mike', age: 32};
-    return copy;
+    let f = 1;
+    n = n * factorial(n - 1);
+    return n;
 }
 
-console.log(transferWaitors(restorantData));
-console.log(restorantData);
+console.log(factorial(5));
