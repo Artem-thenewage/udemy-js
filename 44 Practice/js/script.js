444f5cc852a6952d203beee009fcf7f35c7fbe20
Artemiy <craftsman2004@mail.ru>
2022-08-19 16:32:38 +0300

    48. Практика. Используем события на странице проекта

--- 44 Practice/js/script.js
+++ 44 Practice/js/script.js
@@ -28,8 +28,56 @@ document.querySelector(".promo__adv").remove();
 document.querySelector(".promo__genre").innerHTML = "ДРАМА";
 document.querySelector(".promo__bg").style.backgroundImage = "url('img/bg.jpg')";
 
-const listItem = document.querySelectorAll(".promo__interactive-item");
 
-movieDB.movies.sort().forEach( (item, i) => {
-    listItem[i].textContent = (i+1) + " " +item;
-});
+function submitMovie(event){
+    event.preventDefault();
+    if (inp.value.trim().length > 21) {
+        movieDB.movies.push(inp.value.substring(0, 21) + '...');
+    }
+    else if(inp.value.trim().length > 0) {
+        movieDB.movies.push(inp.value);
+    }
+    else {
+        return '';
+    }
+    inp.value = '';
+    showMovieList();
+    //console.log(movieDB.movies);
+}
+
+function showMovieList() {
+    const movieList = document.querySelector('.promo__interactive-list');
+
+    movieList.innerHTML = "";
+    movieDB.movies.sort();
+    movieDB.movies.forEach((film, i) => {
+        movieList.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+    document.querySelectorAll('.delete').forEach( (btnDel, i) => {
+        btnDel.addEventListener('click', () => {
+            btnDel.parentElement.remove();
+            movieDB.movies.splice(i,1);
+            showMovieList();
+            console.log(movieDB.movies);
+        }
+        );
+    }
+    );
+}
+function deleteMovie(e){
+    alert(e);
+}
+const inp = document.querySelector('.adding__input');
+const btn = document.querySelector('.add>button');
+let deleteButtons = document.querySelectorAll('.delete');
+//console.log('');
+
+
+
+
+btn.addEventListener('click', submitMovie );
+showMovieList();
