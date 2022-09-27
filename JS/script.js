732a748e303d5f06e3585f2578069c6c38f895cb
Artemiy <craftsman2004@mail.ru>
2022-09-27 22:29:45 +0300

    Coding Exercise 21: Задания на работу с методами массивов

--- JS/script.js
+++ JS/script.js
@@ -1,70 +1,48 @@
 // Задачи:
-// 1) У вас есть список фильмов с рейтингом в виде массива объектов. Напишите функцию showGoodFilms, которая будет принимать этот массив, 
-// а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
-// P.S. Это довольно типичная задача в программировании. Вспомните, на самых разных сайтах можно так фильтровать любые товары/фильмы/сериалы...
-// 2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит названия фильмов через запятую.
+// 1) У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, 
+// которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
 // Пример:
-// showListOfFilms(films) => "Titanic, Die hard 5, Matrix, Some bad film"
-// 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но у каждого фильма будет 
-// новое поле id. Значение этого поля установите по нумерации фильма.
+// getPositiveIncomeAmount(funds) => 13300
+// 2) Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов 
+// содержит отрицательное значение поля amount, то функция возвращает сумму всех значений. (число) Если таких значений 
+// нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
 // Пример:
-// setFilmsIds(films)  => [   { name: 'Titanic', rating: 9, id: 0 },   { name: 'Die hard 5', rating: 5, id: 1 },   { name: 'Matrix', rating: 8, id: 2 },   
-// { name: 'Some bad film', rating: 4, id: 3 } ]
-// 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, что в каждом из 
-// фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, если мы передаем checkFilms(tranformedArray); :)
-// P.S. Вот тут вы столкнетесь с интересным моментом, который я хочу, чтобы вы запомнили. Внимательно проследите за тем, что происходит внутри коллбэка и
-//  что будет проверяться. Дополнительно расписал этот момент в комментариях в ответах.
+// getTotalIncomeAmount(funds) => -500
+
+
 'use strict';
-const films = [
-    {
-        name: 'Titanic',
-        rating: 9
-    },
-    {
-        name: 'Die hard 5',
-        rating: 5
-    },
-    {
-        name: 'Matrix',
-        rating: 8
-    },
-    {
-        name: 'Some bad film',
-        rating: 4
-    }
+// const funds = [
+//     {amount: 1400},
+//     {amount: 2400},
+//     {amount: 1000},
+//     {amount: 500},
+//     {amount: 10400},
+//     {amount: 11400}
+// ];
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
 ];
+const getPositiveIncomeAmount = (data) => {
+    const res = data.filter( (item) => item.amount > 0).reduce((sum, current) => sum + current.amount, 0);
+    return res;
 
-function showGoodFilms(arr) {
-    return arr.filter( (obj) => obj.rating >= 8 );
-}
-//console.log(showGoodFilms(films));
-
-
-function showListOfFilms(arr) {
-    let sum = arr[0].name;
-    const newarr = arr.reduce( (sum, curr) => `${typeof(sum) == 'object' ? sum.name : sum}, ${curr.name}`);
-    return newarr;
-}
-//console.log(showListOfFilms(films));
-
-function setFilmsIds(arr) {
-    return arr.map( (obj, index) => {
-        obj.id = index;
-        return obj;}
-    );
-}
-//console.log(setFilmsIds(films));
-
-const tranformedArray = setFilmsIds(films);
-
-function checkFilms(arr) {
-    // const newArr = Object.entries(arr);
-    // console.log(newArr);
-    return arr.every(item => typeof(item.id) != 'undefined');
-}
-
-//console.log(checkFilms(tranformedArray));
+};
+//console.log(getPositiveIncomeAmount(funds));
 
 
+const getTotalIncomeAmount = (data) => {
 
+    if (data.find( (el) => el.amount < 0 ? true : false)) {
+        return data.reduce( (sum, current) => sum + current.amount, 0);
+    }
+    else {
+        return getPositiveIncomeAmount(data);
+    }
 
+};
+console.log(getTotalIncomeAmount(funds));
